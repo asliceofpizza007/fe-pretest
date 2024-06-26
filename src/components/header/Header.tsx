3fe663570e0d4c43ea1f119a546f6012e541a446
asliceofpizza007 <a89884526@hotmail.com.tw>
2024-06-26 12:01:00 +0800

    [chore] remove `Header` component

--- src/components/header/Header.tsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <header className="sticky top-0 flex items-center border-b-[#ffffff1a] border-solid h-10 z-50 xl:h-14">
-      Header
-    </header>
-  );
-};
-
-export default Header;
